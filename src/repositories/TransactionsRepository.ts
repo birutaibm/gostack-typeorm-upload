89f689cca09c05846830432e48c0392d8249b36e
Rafael Arantes <birutaibm@gmail.com>
2020-04-20 23:19:16 -0300

    Finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,22 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const result = transactions.reduce(
+      (balance, transaction) => {
+        let { income, outcome, total } = balance;
+        if (transaction.type === 'income') {
+          income += transaction.value;
+          total += transaction.value;
+        } else {
+          outcome += transaction.value;
+          total -= transaction.value;
+        }
+        return { income, outcome, total };
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+    return result;
   }
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,17 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import Transations from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const repository = getCustomRepository(Transations);
+    const transation = await repository.findOne(id);
+    if (!transation) {
+      throw new AppError('Transaction not found', 404);
+    } else {
+      await repository.delete(id);
+    }
   }
 }
 
